88a11bb421a6118b4dda1d780f1db36b96e66722
System Systems <systems@example.com>
2025-10-27 09:40:20 -0500

    feat: deliver sprint 18 progression components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 export * from './RenderObject.js';
 export * from './base/index.js';
 export * from './page/index.js';
+export * from './progress/index.js';
+export * from './stepper/index.js';
 export { OODS } from './OODS.js';
