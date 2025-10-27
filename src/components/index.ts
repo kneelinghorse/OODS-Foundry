8a181a0f3c43720d148aa176fcd3632b00da95f4
System Systems <systems@example.com>
2025-10-27 12:41:39 -0500

    feat(tabs): add responsive navigation tabs

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export * from './base/index.js';
 export * from './page/index.js';
 export * from './progress/index.js';
 export * from './stepper/index.js';
+export * from './tabs/index.js';
 export { OODS } from './OODS.js';
